237fad113b8c183f7065e17c83022ca1fd4fbca0
aolksa <159139032+aolksa@users.noreply.github.com>
2024-02-06 14:59:31 +0300

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,16 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6lQU0Z1QrYf":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  document.getElementById("frame").style.backgroundColor = "#ffffff";
+
+}
+
